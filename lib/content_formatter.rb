ab9139c22d19e183e0ae59818f4056c06c04b7d0
Dinesh Chohda <dinesh.skyach@gmail.com>
2018-10-08 16:57:37 +0530

    changed to sanitize

--- lib/content_formatter.rb
+++ lib/content_formatter.rb
@@ -21,6 +21,6 @@ class ContentFormatter
     body_html = auto_link(@body) { |text| truncate(text, :length => 50) }
     textilized = RedCloth.new(body_html, [ :hard_breaks ])
     textilized.hard_breaks = true if textilized.respond_to?("hard_breaks=")
-    white_list(textilized.to_html)
+    sanitize(textilized.to_html)
   end
 end
